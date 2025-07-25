20088826d370857ef94a1d30a84921af46fea4d6
Sai Kumar Erumalla <saikumar.erumalla@gmail.com>
2025-07-25 21:17:25 +0900

    Student Controller Added

---
+++ SpringBatchProcessing/src/main/java/com/explore/SpringBatchProcessing/student/StudentController.java
@@ -0,0 +1,41 @@
+package com.explore.SpringBatchProcessing.student;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    public StudentController(Job job, JobLauncher jobLauncher) {
+        this.job = job;
+        this.jobLauncher = jobLauncher;
+    }
+
+    @PostMapping
+    public void importCsvtoDB() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis())
+                .toJobParameters();
+
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException
+                 | JobRestartException
+                 | JobInstanceAlreadyCompleteException
+                 | JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+}
