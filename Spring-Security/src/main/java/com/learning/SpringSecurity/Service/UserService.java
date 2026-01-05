9751f05c9adda82987ae863095f71f715b64ceaf
Sai Kumar Erumalla <saikumar.erumalla@gmail.com>
2026-01-05 21:42:53 +0900

    Password hashing Added

--- Spring-Security/src/main/java/com/learning/SpringSecurity/Service/UserService.java
+++ Spring-Security/src/main/java/com/learning/SpringSecurity/Service/UserService.java
@@ -3,6 +3,7 @@ package com.learning.SpringSecurity.Service;
 import com.learning.SpringSecurity.Dao.User;
 import com.learning.SpringSecurity.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,10 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public User addUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 }
