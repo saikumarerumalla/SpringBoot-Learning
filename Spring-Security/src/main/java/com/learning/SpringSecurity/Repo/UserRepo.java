23148a45190339c85dfdc1b0144e943b8765cf35
Sai Kumar Erumalla <saikumar.erumalla@gmail.com>
2026-01-07 22:25:04 +0900

    User Repo added

--- Spring-Security/src/main/java/com/learning/SpringSecurity/Repo/UserRepo.java
+++ Spring-Security/src/main/java/com/learning/SpringSecurity/Repo/UserRepo.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     boolean existsByEmail (String email);
     Optional<User> findByEmail (String email);
 
