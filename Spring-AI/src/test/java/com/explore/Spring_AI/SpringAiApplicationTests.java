7c7e5cd55bb8adbdaeb7ea80312ecffbdcb082e6
Sai Kumar Erumalla <saikumar.erumalla@gmail.com>
2026-01-17 01:19:22 +0900

    Added Spring AI Module

---
+++ Spring-AI/src/test/java/com/explore/Spring_AI/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.Spring_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
